6cecada4f74edf30ea857c6bcb4667a82846b928
arun <arunrv2011@gmail.com>
2015-08-25 00:13:31 +0530

    p03 - get the element at position k in a list

--- p01-p10.fs
+++ p01-p10.fs
@@ -1,14 +1,24 @@
 module fsharp99
 
+(* P01 - find the last element of a list *)
 let rec myLast ys = 
     match ys with
     | [] -> failwith "No last element in an empty list!"
     | [x] -> x
     | _::xs -> myLast xs
 
+(* P02 - find the last but one element of a list *)
 let rec myButLast ys =
     match ys with
     | []  -> failwith "No last but one element in an empty list"
     | [x] -> failwith "No last but one element in a list with only one element"
     | [x;_] -> x
     | _::xs -> myButLast xs
+
+(* P03 - find the kth element of a list *)
+let rec elementAt k ys = 
+    match (k, ys) with 
+    | (k, _) when k <= 0 -> failwith "Invalid index requested"
+    | (_, []) -> failwith "List is too small"
+    | (1, x::xs) -> x
+    | (k, x::xs) -> elementAt (k - 1) xs 
