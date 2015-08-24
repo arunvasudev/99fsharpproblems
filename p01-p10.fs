815ce97337b88f721d82a3998a982881bf06e3e9
arun <arunrv2011@gmail.com>
2015-08-24 09:27:26 +0530

    p02 - find the last but one element of a list

--- p01-p10.fs
+++ p01-p10.fs
@@ -5,3 +5,10 @@ let rec myLast ys =
     | [] -> failwith "No last element in an empty list!"
     | [x] -> x
     | _::xs -> myLast xs
+
+let rec myButLast ys =
+    match ys with
+    | []  -> failwith "No last but one element in an empty list"
+    | [x] -> failwith "No last but one element in a list with only one element"
+    | [x;_] -> x
+    | x::xs -> myButLast xs
