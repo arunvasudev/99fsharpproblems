b9cd7072b17c6257b2d7bdd8b1a3cad747b8efd7
arun <arunrv2011@gmail.com>
2015-08-27 13:35:12 +0530

    p16 - drop every nth element of a list

--- p14-p20.fs
+++ p14-p20.fs
@@ -6,10 +6,30 @@ let rec dupli xs =
     | [] -> []
     | x::xs' -> x::x::(dupli xs')
 
-(* p15 - Replicate elements of a list a given number of times *)
-let rec repli n xs = 
+(* P15 - Replicate elements of a list a given number of times *)
+let repli n xs = 
     let rec repli' n xs = 
         match xs with
         | [] -> []
         | x::xs' -> (List.replicate n x)::(repli' n xs')
     List.concat (repli' n xs)
+
+(* P16 - Drop every nth element of a list *)
+let rec drop n xs =
+    match (n, xs) with
+    | (n, _) when n <= 0 -> xs
+    | (_, []) -> []
+    | (_, x::xs') -> drop (n-1) xs'
+
+let rec take n xs =
+    match (n, xs) with
+    | (n, _) when n <= 0 -> []
+    | (_, []) -> []
+    | (n, x::xs') -> x::(take (n-1) xs')
+
+let dropNth n xs = 
+    let rec dropNth' n xs =
+        match xs with 
+        | [] -> []
+        | _ -> (take (n - 1) xs)::(dropNth' n (drop n xs))
+    List.concat (dropNth' n xs)
