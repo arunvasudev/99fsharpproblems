31c90299ca7a689e79105361ce928779851f27dd
arun <arunrv2011@gmail.com>
2015-08-27 13:19:30 +0530

    P15 - Replicate elements of a list a given number of times

--- p14-p20.fs
+++ p14-p20.fs
@@ -5,3 +5,11 @@ let rec dupli xs =
     match xs with
     | [] -> []
     | x::xs' -> x::x::(dupli xs')
+
+(* p15 - Replicate elements of a list a given number of times *)
+let rec repli n xs = 
+    let rec repli' n xs = 
+        match xs with
+        | [] -> []
+        | x::xs' -> (List.replicate n x)::(repli' n xs')
+    List.concat (repli' n xs)
