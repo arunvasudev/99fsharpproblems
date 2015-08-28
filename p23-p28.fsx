0f8b5d7a1b3613f145b4b0869f40a021921ffc5d
arun <arunrv2011@gmail.com>
2015-08-28 08:19:55 +0530

    P24, P25

--- p23-p28.fsx
+++ p23-p28.fsx
@@ -5,13 +5,13 @@ module fsharp99
 (* Returns a list of count no. of random numbers between minVal and maxVal inclusive *)
 let genRandomInts1 count minVal maxVal = 
     let r = new System.Random()
-    [for i in 1..count do yield r.Next(minVal, maxVal) ]
+    [for i in 1..count do yield r.Next(minVal, maxVal + 1) ]
     
 let genRandomInts2 count minVal maxVal =
     let r = new System.Random()
-    [for i in 1..count do yield r.Next(minVal, maxVal - i + 1)]
+    [for i in 1..count do yield r.Next(minVal, maxVal - i + 2)]
 
-(* P24 - Extracts a n elements from a list randomly *)
+(* P23 - Extracts a n elements from a list randomly *)
 let rndSelect (xs:'a list) (n:int) = 
     let rec rndSelect' rands xs n =
         match rands, xs, n with
@@ -20,3 +20,9 @@ let rndSelect (xs:'a list) (n:int) =
         | r::rands', xs, n  -> (elementAt r xs)::(rndSelect' rands' (removeNth r xs) (n-1))
     let rands = genRandomInts2 n 1 (List.length xs)
     rndSelect' rands xs n
+
+(* P24 - Select n numbers in the range 1 .. m randomly *)
+let rndSelectInRange n m = rndSelect [1..m] n
+
+(* P25 - Generate a permutation of a given list *)
+let rndPermute xs = rndSelect xs (List.length xs)
