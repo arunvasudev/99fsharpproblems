686dcd58b550a9047159742fc6703eea92783329
arun <arunrv2011@gmail.com>
2015-08-30 10:47:05 +0530

    P32 - GCD of two numbers

--- p31-p41.fsx
+++ p31-p41.fsx
@@ -1,6 +1,12 @@
 module fsharp99
 
-(* P31 - Returns true if n is a prime *)
+(* P31 - returns true if n is a prime *)
 let isPrime n =
     let intSqrt = int (ceil (sqrt (float n)))
     n = 2 || not (List.exists (fun d -> n % d = 0) [2..intSqrt])
+
+(* P32 - returns the GCD of two numbers *)
+let rec gcd a b = 
+    match b with 
+    | 0 -> a
+    | _ -> gcd b (a % b)
