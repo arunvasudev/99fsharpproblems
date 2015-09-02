ab196377c2244e38cf69cd4e54e0534da3c057da
arun <arunrv2011@gmail.com>
2015-09-03 00:25:02 +0530

    P46 - generate truth tables for general boolean expressions

--- p46-p50.fsx
+++ p46-p50.fsx
@@ -18,3 +18,34 @@ let table func =
         | [] -> ()
     eval inps
                     
+
+(* For quickly testing P48 *)
+let exp2 xs = 
+    match xs with
+    | [x; y] -> And x y
+    | [x; y; z] -> And (Or x y) (Not z)
+    | _ -> failwith "No expression available for that list"
+
+(* P48 - truth tables for expressions in general *)
+let tablen n func =
+    let rec genInputs n =
+        match n with
+        | 1 -> [[true];[false]]
+        | _ -> let inps' = genInputs (n-1)
+               [for inp in inps' do yield true::inp] @ [for inp in inps' do yield false::inp]
+
+    let rec eval inps =
+        match inps with
+        | inp::inps' ->
+            let rec printInp inp' =
+                match inp' with
+                | t::inp'' -> printf "%A " t
+                              printInp inp''
+                | [] -> printfn "%A" (func inp)
+
+            printInp inp
+            eval inps'
+
+        | [] -> ()
+
+    eval (genInputs n) 
