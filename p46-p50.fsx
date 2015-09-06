28b637acec2080c2bdb4190a54f90089d8597541
arun <arunrv2011@gmail.com>
2015-09-07 03:12:48 +0530

    p50 - huffman coding

--- p46-p50.fsx
+++ p46-p50.fsx
@@ -63,3 +63,57 @@ let rec gray n cache =
                    let cache'' = Map.add n newGray cache'
                    (newGray, cache'')
         gray' n cache
+
+(* P50 - Huffman coding *)
+type Tree<'a> = 
+    | Leaf of value:'a * freq:int 
+    | Branch of left:Tree<'a> * right:Tree<'a> * freq:int
+
+let freq t =
+    match t with
+    | Leaf (v, f) -> f
+    | Branch (l, r, f) -> f
+
+(* Inserts a tree t into a sorted list of trees ts *)
+let rec insertSorted (t:Tree<'a>) ts =
+    match ts with 
+    | [] -> [t]
+    | t'::ts' -> 
+        let (ft, ft') = (freq t, freq t')
+        if (ft < ft') then t::ts
+        else t'::(insertSorted t ts')
+
+(* encodes a list of pairs of the form (value, frequency) into 
+  the corresponding huffman coding of the form (value, code) *)
+let huffman freqs = 
+    let rec freqsToNodes fs =
+        match fs with
+        | [] -> []
+        | (v, f)::fs' -> insertSorted (Leaf(v, f)) (freqsToNodes fs')
+
+    let rec combineNodes ts =
+        match ts with 
+        | [] -> failwith "Unexpected empty list encountered in combineNodes"
+        | [t] -> [t]
+        | t1::t2::ts' -> 
+            let (f1, f2) = (freq t1, freq t2)
+            let fnew = f1 + f2
+            let tnew = if (f1 < f2) then Branch(t1, t2, fnew) else Branch(t2, t2, fnew)
+            let ts'' = insertSorted tnew ts'
+            combineNodes ts''
+
+    let rec encodeTree t code =
+       match t with
+       | Leaf(v, _) -> [(v, code)]
+       | Branch(l, r, _) -> 
+            let leftCodes = encodeTree l (code + "0")
+            let rightCodes = encodeTree r (code + "1")
+            leftCodes @ rightCodes
+
+    let ts = freqsToNodes freqs 
+    let fts = combineNodes ts
+    match fts with
+    | [ft] -> encodeTree ft ""
+    | _ -> failwith "Expected a singleton list with one tree, got something else."
+
+let huffTest1 = [('a',45);('b',13);('c',12);('d',16);('e',9);('f',5)]
